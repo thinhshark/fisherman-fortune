9662a77a1c5f23ec3023e769006ff2a60b442016
Thinh Nguyen <thinhshark@gmail.com>
2026-09-04 16:22:12 +0700

    Build static fishing scene

--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -19,17 +19,59 @@ export default class Level extends Phaser.Scene {
 
 	editorCreate(): void {
 
-		// fufuSuperDino
-		this.add.image(640, 257, "FufuSuperDino");
+		// gameBackground
+		const gameBackground = this.add.image(642, 295.3333333333333, "game-bg");
+		gameBackground.scaleX = 0.9548611111111112;
+		gameBackground.scaleY = 0.8950892857142857;
 
-		// text
-		const text = this.add.text(640, 458, "", {});
-		text.setOrigin(0.5, 0.5);
-		text.text = "Phaser 4 + Phaser Editor v5\nVite + TypeScript";
-		text.setStyle({ "align": "center", "fontFamily": "Arial", "fontSize": "3em" });
+		// water
+		const water = this.add.image(650.6666666666666, 276, "bg");
+		water.scaleX = 0.6840277777777778;
+		water.scaleY = 0.7015625;
+		water.setOrigin(0.5, 0.01687116564417178);
+
+		// player
+		const player = this.add.image(638.9688750209934, 289.0012601206786, "fisherman-idle");
+		player.scaleX = 0.72842474;
+		player.scaleY = 0.72842474;
+		player.setOrigin(0.676, 0.8228228228228228);
+
+		// rope
+		const rope = this.add.image(640, 289.48717830155135, "hook-sine");
+		rope.scaleX = 0.675929855162;
+		rope.scaleY = 0.660344053;
+		rope.setOrigin(0.5, 0);
+
+		// hookLeft
+		const hookLeft = this.add.image(632.9036995365968, 347.1169994301449, "hook-left");
+		hookLeft.scaleX = 0.6666666666666666;
+		hookLeft.scaleY = 0.6666666666666666;
+		hookLeft.angle = 5;
+		hookLeft.setOrigin(0.78125, 0.146341);
+
+		// hookRight
+		const hookRight = this.add.image(646.1641161388904, 347.01283278922745, "hook-right");
+		hookRight.scaleX = 0.6666666666666666;
+		hookRight.scaleY = 0.6666666666666666;
+		hookRight.angle = 355;
+		hookRight.setOrigin(0.171875, 0.146341);
+
+		this.gameBackground = gameBackground;
+		this.water = water;
+		this.player = player;
+		this.rope = rope;
+		this.hookLeft = hookLeft;
+		this.hookRight = hookRight;
 
 		this.events.emit("scene-awake");
 	}
+
+	public gameBackground!: Phaser.GameObjects.Image;
+	public water!: Phaser.GameObjects.Image;
+	public player!: Phaser.GameObjects.Image;
+	public rope!: Phaser.GameObjects.Image;
+	public hookLeft!: Phaser.GameObjects.Image;
+	public hookRight!: Phaser.GameObjects.Image;
 
 	/* START-USER-CODE */
 
